5a6fc42b4ae47ab95c1c91ee742fdb7dcfa6df38
AllanKariuki <a.kariuki@keshiholdings.com>
2025-08-22 11:00:00 +0300

    feat: beauty salon page

---
+++ src/pages/BeautySalon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BeautySalonLanding from "../components/BeautySalonLanding";
+
+const BeautySalon: React.FC = () => {
+  return (
+    <div className="min-h-screen">
+      <BeautySalonLanding />
+    </div>
+  );
+};
+
+export default BeautySalon;
