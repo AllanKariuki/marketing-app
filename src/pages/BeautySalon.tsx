e3827c57f7f50cbdc4d765730c43228f7e36b8b0
AllanKariuki <a.kariuki@keshiholdings.com>
2025-08-28 10:57:16 +0300

    feat: add the services section

--- src/pages/BeautySalon.tsx
+++ src/pages/BeautySalon.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BeautySalonLanding from "../components/BeautySalonLanding";
+import OurServices from "../components/OurServices";
 
 const BeautySalon: React.FC = () => {
   return (
     <div className="min-h-screen">
       <BeautySalonLanding />
+      <OurServices />
     </div>
   );
 };
